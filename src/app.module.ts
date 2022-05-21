9abc1b6c6be5195ef9b3d710e1665055ecf2a0c5
rizzalbayu <rizzalbayu21@gmail.com>
2022-05-21 11:02:58 +0700

    modify app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MenuModule } from './menu/menu.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({})],
+  imports: [TypeOrmModule.forRoot({}), MenuModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
